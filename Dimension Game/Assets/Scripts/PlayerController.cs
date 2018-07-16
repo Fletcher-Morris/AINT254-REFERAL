60e638524fd7d120a70a7a24ea5342bd42f8fcaf
Fletcher Morris <odmoandtheboy@gmail.com>
2018-07-16 18:46:02 +0100

    Added a few variables to player controller
    Added variables for adjusting movement speed, jump force, sensitivity, etc.

--- Dimension Game/Assets/Scripts/PlayerController.cs
+++ Dimension Game/Assets/Scripts/PlayerController.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
 
+    //  References to various instances
     private Transform m_transform;
     private Camera m_cam;
     private Transform m_camTransform;
     private Rigidbody m_body;
+    private CapsuleCollider m_col;
+    private Transform m_groundCheck;
 
+    //  Movement settings
+    [SerializeField]
+    private float m_runSpeed = 5f;
+    [SerializeField]
+    private float m_jumpForce = 2f;
+    [SerializeField]
+    private float m_lookSensitivity = 1f;
 
 
     private void Start()
@@ -22,5 +33,8 @@ public class PlayerController : MonoBehaviour {
         if (!m_cam) m_cam = GetComponentInChildren<Camera>();
         m_camTransform = m_cam.transform;
         m_body = GetComponent<Rigidbody>();
+        m_col = GetComponent<CapsuleCollider>();
+
+        m_body.freezeRotation = true;
     }
 }
