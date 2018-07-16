618f5e043221669fcadd94867ffb724082703e51
Fletcher Morris <odmoandtheboy@gmail.com>
2018-07-16 18:20:05 +0100

    Added Dimension enum
    Added an enum to help coordinate dimensions.

--- Dimension Game/Assets/Scripts/DimensionLayerController.cs
+++ Dimension Game/Assets/Scripts/DimensionLayerController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class DimensionLayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+}
+
+public enum Dimension
+{
+    Normal,
+    Other
 }
