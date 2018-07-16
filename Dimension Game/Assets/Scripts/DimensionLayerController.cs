ab620fe879c7f458a33802497bc36dbdbac0ea73
Fletcher Morris <odmoandtheboy@gmail.com>
2018-07-16 18:38:07 +0100

    Added 'DimensionDef' class
    Added a class for defining the visible layers, and other attributes of each dimension.

--- Dimension Game/Assets/Scripts/DimensionLayerController.cs
+++ Dimension Game/Assets/Scripts/DimensionLayerController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DimensionLayerController : MonoBehaviour {
-
-	
+    
+    public DimensionDef[] dimensionDefinitions;
 }
 
 public enum Dimension
 {
-    Normal,
-    Other
+    Normal = 0,
+    Other = 1
+}
+
+[System.Serializable]
+public class DimensionDef
+{
+    public string name = "Dimension";
+    public LayerMask visibleLayers;
 }
