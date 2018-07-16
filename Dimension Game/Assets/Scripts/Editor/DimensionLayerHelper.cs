0b76b3513d45c8e17541b451fc7506f06e7f71db
Fletcher Morris <odmoandtheboy@gmail.com>
2018-07-17 00:41:23 +0100

    Editor window can now gather lights, cameras, and gameobjects

--- Dimension Game/Assets/Scripts/Editor/DimensionLayerHelper.cs
+++ Dimension Game/Assets/Scripts/Editor/DimensionLayerHelper.cs
@@ -13,8 +13,47 @@ public class DimensionLayerHelper : EditorWindow {
 
     Dimension m_sceneDimension;
 
+    List<GameObject> normalObjects;
+    List<Camera> cameraObjects;
+    List<Light> lightObjects;
+
     private void OnGUI()
     {
         m_sceneDimension = (Dimension)EditorGUILayout.EnumPopup("Scene's Dimension", m_sceneDimension);
+        if(GUILayout.Button("Gather Objects"))
+        {
+            GatherObjects();
+        }
+    }
+
+    private void GatherObjects()
+    {
+        normalObjects = new List<GameObject>();
+        cameraObjects = new List<Camera>();
+        lightObjects = new List<Light>();
+
+        foreach(GameObject go in UnityEngine.Object.FindObjectsOfType<GameObject>())
+        {
+            if(go.GetComponent<Camera>())
+            {
+                cameraObjects.Add(go.GetComponent<Camera>());
+            }
+
+            if(go.GetComponent<Light>())
+            {
+                lightObjects.Add(go.GetComponent<Light>());
+            }
+
+            normalObjects.Add(go);
+        }
+
+        Debug.Log(normalObjects.Count + " GameObjects gathered.");
+        Debug.Log(cameraObjects.Count + " Cameras gathered.");
+        Debug.Log(lightObjects.Count + " Lights gathered.");
+    }
+
+    private void ConvertLayers(Dimension dim)
+    {
+
     }
 }
