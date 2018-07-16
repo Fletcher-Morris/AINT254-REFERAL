be8d5848fb26340ebaf8e2c9d77f3546969f08eb
Fletcher Morris <odmoandtheboy@gmail.com>
2018-07-17 00:21:02 +0100

    Added 'DimensionLayerHelper' script
    Added an editor script to assist me in assigning the correct layers and attributes to scenes.

---
+++ Dimension Game/Assets/Scripts/Editor/DimensionLayerHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DimensionLayerHelper : EditorWindow {
+
+	[MenuItem("Dimensions/Dimension Layer Helper")]
+    public static void ShowWindow()
+    {
+        GetWindow<DimensionLayerHelper>("Layer Helper");
+    }
+}
