e7f22d19064e3fb35a4721165631c7ad5b2d3ab7
Fletcher Morris <odmoandtheboy@gmail.com>
2018-07-18 17:36:06 +0100

    Created singletons class
    Created a static class called 'singletons', which stores instances of scripts, in order to treat them as singletons.

---
+++ Dimension Game/Assets/Scripts/GameManagerSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSingleton : MonoBehaviour {
+
+    private void Start()
+    {
+        if(Singletons.gameManager == null)
+        {
+            Singletons.gameManager = this;
+        }
+        else
+        {
+            if(Singletons.gameManager != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
+public static class Singletons
+{
+    public static GameManagerSingleton gameManager;
+}
