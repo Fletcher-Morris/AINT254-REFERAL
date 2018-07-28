46239a486694260c039e7bd447638f30cc745c55
Fletcher Morris <odmoandtheboy@gmail.com>
2018-07-28 22:15:11 +0100

    Created DimensionPortal script
    Created a script to manage an individual dimensional tear/portal;

---
+++ Dimension Game/Assets/Scripts/DimnsionPortal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiensionPortal : MonoBehaviour {
+
+    [SerializeField]
+    private Dimension m_showDimension;
+
+    private Transform m_transform;
+    private Transform m_playerTransform;
+    [SerializeField]
+    private Collider m_collider;
+    [SerializeField]
+    private Renderer m_renderer;
+
+    private void Start()
+    {
+        InitPortal();
+    }
+
+    public void InitPortal()
+    {
+        m_transform = GetComponent<Transform>();
+        m_playerTransform = GameObject.Find("Player").transform;
+        m_collider = GetComponent<Collider>();
+    }
+}
