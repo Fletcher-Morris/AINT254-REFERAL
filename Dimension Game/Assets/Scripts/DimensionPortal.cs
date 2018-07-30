b8b05033f252a07e6001ef3f3d96a94eee4a7891
Fletcher Morris <odmoandtheboy@gmail.com>
2018-07-30 17:17:52 +0100

    Added 'OpenPortalCouroutine' to portal script

--- Dimension Game/Assets/Scripts/DimensionPortal.cs
+++ Dimension Game/Assets/Scripts/DimensionPortal.cs
@@ -23,18 +23,46 @@ public class DimensionPortal : MonoBehaviour {
 
     public float currentRange = 100f;
 
+    public float openTime = 0.5f;
+
     private void Start()
     {
         InitPortal();
     }
 
-    public void InitPortal()
+    private void InitPortal()
     {
         m_transform = GetComponent<Transform>();
         m_playerTransform = GameObject.Find("Player").transform;
         m_collider = GetComponent<Collider>();
 
         initialised = true;
+    }
+
+    public void OpenPortal(Dimension toDimension)
+    {
+
+    }
+
+    private IEnumerator OpenPortalCoroutine(Dimension toDimension)
+    {
+        float t = 0.0f;
+        float completion = 0.0f;
+        bool open = false;
+
+        while(!open)
+        {
+            t += Time.deltaTime;
+            completion = Mathf.Clamp01(t / openTime);
+
+            m_renderer.material.SetFloat("_Completion", completion);
+
+            if (completion >= 1.0f) open = true;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return null;
     }
 
     private void Update()
