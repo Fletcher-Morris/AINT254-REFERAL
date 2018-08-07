3b55bc56487f3e534beda16d4aa03d00ef34257b
Fletcher Morris <odmoandtheboy@gmail.com>
2018-08-07 17:11:22 +0100

    Fixed diamond bobbing

--- Dimension Game/Assets/Scripts/Gem.cs
+++ Dimension Game/Assets/Scripts/Gem.cs
@@ -8,56 +8,59 @@ public class Gem : MonoBehaviour {
     private int m_value;
     public Dimension dimension;
     [SerializeField]
-    private bool m_rotate = true;
-    [SerializeField]
     private bool m_bob = true;
     [SerializeField]
     private bool m_collectable = true;
 
     private Transform m_transform;
-    private Transform m_player;
+    private PlayerController m_player;
+    private Vector3 m_originalPosition;
     private float m_movementRange = 2f;
     private float m_collectionRange = 0.25f;
     private float m_currentRange;
 
     private Vector3 m_rotationAxis;
-    private float m_rotateSpeed = 10.0f;
-    private float m_bobRange = 0.5f;
+    private float m_bobRange = 0.25f;
+    [SerializeField]
     private float m_bobSpeed = 1.0f;
+
+    private bool m_isBeingCollected = false;
 
     public float t = 0;
 
     private void Start()
     {
         m_transform = GetComponent<Transform>();
-        m_player = GameObject.Find("Player").transform;
+        m_player = GameObject.Find("Player").GetComponent<PlayerController>();
+        m_originalPosition = m_transform.position;
         m_rotationAxis = m_transform.up;
     }
 
     private void Update()
     {
-        if(m_rotate)
-        {
-            m_transform.Rotate(m_rotationAxis, m_rotateSpeed * Time.deltaTime);
-        }
-
-        Vector3 newPos = m_transform.position;
-
-        if (m_bob)
-        {
-            t += m_bobRange * Mathf.Sin(m_bobSpeed * Time.deltaTime);
-            newPos.y = t;
-        }
+        Vector3 newPos = m_originalPosition;
 
         if(m_player && m_collectable)
         {
-            m_currentRange = Vector3.Distance(m_transform.position, m_player.position);
+            m_currentRange = Vector3.Distance(m_transform.position, m_player.cameraAnchor.transform.position);
 
             if(m_currentRange <= m_movementRange)
             {
-                newPos = Vector3.MoveTowards(m_transform.position, m_player.position, 10 * Time.deltaTime);
+                newPos = Vector3.MoveTowards(m_transform.position, m_player.cameraAnchor.transform.position, 10 * Time.deltaTime);
+                m_isBeingCollected = true;
                 if (m_currentRange <= m_collectionRange) CollectGem();
             }
+            else
+            {
+                m_isBeingCollected = false;
+            }
+        }
+
+        if (!m_isBeingCollected && m_bob)
+        {
+            newPos = m_originalPosition;
+            newPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * m_bobSpeed) * m_bobRange;
+            m_transform.position = newPos;
         }
 
         m_transform.position = newPos;
