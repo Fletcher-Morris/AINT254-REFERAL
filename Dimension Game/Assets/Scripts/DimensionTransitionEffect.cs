efd95eff0299b1e4a473df5938cdcf57e0a8a490
Fletcher Morris <odmoandtheboy@gmail.com>
2018-08-16 03:01:15 +0100

    Commented code
    Super grass never really worked, got rid of it.
    Commented some more code.

--- Dimension Game/Assets/Scripts/DimensionTransitionEffect.cs
+++ Dimension Game/Assets/Scripts/DimensionTransitionEffect.cs
@@ -5,47 +5,35 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class DimensionTransitionEffect : MonoBehaviour {
 
-    public Shader effectShader;
+    public Shader effectShader;         //  The shader to use
     [Range(0, 1)]
-    public float intensity = 0.5f;
-    public float speed = 30.0f;
-    public Material material;
-
-    [SerializeField]
-    private float t;
-    [SerializeField]
-    private int i;
+    public float intensity = 0.5f;      //  The intesity of the effect
+    public float speed = 30.0f;         //  The speed at which the sprites should change
+    public Material material;           //  The material to use on the camera
+    private float t;                    //  The current time of the shader
+    private int i;                      //  The current sprite to use in the shader
 
     private void Start()
     {
-        //if (effectShader)
-        //{
-        //    material = new Material(effectShader);
-        //}
-        //else
-        //{
-        //    effectShader = Shader.Find("Custom/DimensionTransitionShader");
-        //    material = new Material(effectShader);
-        //}
-
+        //  Set the intensity to 0
         intensity = 0.0f;
     }
 
+    //  Update the shader when the camera renders
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         if(material)
         {
+            //  Increase the time in the shader
             t += speed * Time.deltaTime;
 
             if (t >= 10) t = 0;
             i = Mathf.FloorToInt(t);
-
+            //  Set the shader to use the next sprite
             material.SetInt("_Img", i);
             material.SetFloat("_Intensity", intensity);
 
             Graphics.Blit(source, destination, material);
         }
-
-        //material = new Material(effectShader);
     }
 }
--- Dimension Game/Assets/Scripts/FloatHolder.cs
+++ Dimension Game/Assets/Scripts/FloatHolder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//  A simple class for holding a float (useful for animating a value on another gameobject)
 public class FloatHolder : MonoBehaviour {
 
     public float value = 0f;
